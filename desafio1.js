aefb0f3a9eab41c07475d4905570c55eee5ecaf0
André Santos <andremissional@gmail.com>
2025-05-20 10:21:12 -0300

    desafio01

---
+++ desafio1.js
@@ -0,0 +1,14 @@
+
+// desafio1.js
+// Soma iterativa com laço while
+
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+console.log("Valor final da variável SOMA:", SOMA);
